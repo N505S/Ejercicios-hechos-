2daf520c2e90d1afc7ee974b5bd57141177a88d1
N505S <81101258+N505S@users.noreply.github.com>
2021-03-21 17:26:06 +0100

    Add files via upload

---
+++ orden_porfavor.py
@@ -0,0 +1,25 @@
+
+import random
+
+primero =int(input("dame un numero: "))
+segundo = int(input("otro más uwu: "))
+tercero = int(input("...y otro: "))
+
+if tercero>segundo>primero:
+    print(f"{tercero}>{segundo}>{primero}")
+
+elif tercero>primero>segundo:
+    print(f"{tercero}>{primero}>{segundo}")
+
+elif segundo>tercero>primero:
+    print(f"{segundo}>{tercero}>{primero}")
+
+elif segundo>primero>tercero:
+    print(f"{segundo}>{primero}>{tercero}")
+
+elif primero>segundo>tercero:
+    print(f"{primero}>{segundo}>{tercero}")
+
+elif primero>tercero>segundo:
+    print(f"{primero} > {tercero} > {segundo}")
+
