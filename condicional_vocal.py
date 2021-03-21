107771b5b4d4f6a7eaa496ca7ad81bfb6a40cfd9
N505S <81101258+N505S@users.noreply.github.com>
2021-03-21 17:41:05 +0100

    Add files via upload

---
+++ condicional_vocal.py
@@ -0,0 +1,5 @@
+letra = input("Presiona cualquier tecla: ").lower()
+if letra=='a' or letra=="e" or letra=='i' or letra=="o" or letra=="u":
+    print("¡Enhorabuena, es una vocal!")
+else:
+    print("... oh no es una vocal, python triste :(")
